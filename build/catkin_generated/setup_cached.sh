fa1924774dc4d6179fcb6101ad769f1ccfad34d3
root <gaoxiaoying.kit@com>
2016-09-05 23:10:56 +0200

    test

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/omi/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/omi/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/omi/catkin_ws/devel/lib:/home/omi/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/jade/lib/x86_64-linux-gnu:/opt/ros/jade/lib"
+export PATH="/home/omi/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/omi/catkin_ws/devel/lib/pkgconfig:/home/omi/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/jade/lib/pkgconfig"
+export PWD="/home/omi/catkin_ws/build"
+export PYTHONPATH="/home/omi/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/omi/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/omi/catkin_ws/src:$ROS_PACKAGE_PATH"
